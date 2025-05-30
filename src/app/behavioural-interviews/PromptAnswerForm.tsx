6a24d808730861e7dbfe89ce681d05f59f77e2e5
mattscripted <46871802+mattscripted@users.noreply.github.com>
2025-05-30 17:49:04 -0400

    Populate prompts, and show selected prompt answer

--- src/app/behavioural-interviews/PromptAnswerForm.tsx
+++ src/app/behavioural-interviews/PromptAnswerForm.tsx
@@ -28,7 +28,7 @@ export default function PromptAnswerForm({
           <div className="mb-2 block">
             <Label htmlFor="answer">Answer</Label>
           </div>
-          <Textarea id="answer" rows={4} value={promptAnswer.answer} />
+          <Textarea id="answer" rows={4} value={promptAnswer.answer} readOnly />
         </div>
       )}
       {promptAnswer.type === "star" && (
@@ -37,25 +37,25 @@ export default function PromptAnswerForm({
             <div className="mb-2 block">
               <Label htmlFor="situation">Situation</Label>
             </div>
-            <Textarea id="situation" rows={4} value={promptAnswer.answer.situation} />
+            <Textarea id="situation" rows={4} value={promptAnswer.answer.situation} readOnly />
           </div>
           <div className="max-w-md mb-4">
             <div className="mb-2 block">
               <Label htmlFor="task">Task</Label>
             </div>
-            <Textarea id="task" rows={4} value={promptAnswer.answer.task} />
+            <Textarea id="task" rows={4} value={promptAnswer.answer.task} readOnly />
           </div>
           <div className="max-w-md mb-4">
             <div className="mb-2 block">
               <Label htmlFor="action">Action</Label>
             </div>
-            <Textarea id="action" rows={4} value={promptAnswer.answer.action} />
+            <Textarea id="action" rows={4} value={promptAnswer.answer.action} readOnly />
           </div>
           <div className="max-w-md mb-4">
             <div className="mb-2 block">
               <Label htmlFor="result">Result</Label>
             </div>
-            <Textarea id="result" rows={4} value={promptAnswer.answer.result} />
+            <Textarea id="result" rows={4} value={promptAnswer.answer.result} readOnly />
           </div>
         </>
       )}
--- src/app/behavioural-interviews/page.tsx
+++ src/app/behavioural-interviews/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import {
   Table,
   TableHead,
@@ -14,91 +14,76 @@ import {
 } from "flowbite-react";
 import clsx from "clsx";
 import { FaRegCircleCheck } from "react-icons/fa6";
+import promptAnswersByTheme from "./prompts";
 import PromptAnswerForm from "./PromptAnswerForm";
 import { PromptAnswer } from "@/types/prompt-answer";
 
 export default function BehaviouralInterviewsPage() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  // TODO: Change selected prompt
-  const [selectedPromptAnswer] = useState<PromptAnswer>({
-    type: "star",
-    prompt: "Tell me about a time you had a conflict with a co-worker.",
-    answer: {
-      situation: "",
-      task: "",
-      action: "",
-      result: "",
-    },
-  });
+  const [selectedPromptAnswer, setSelectedPromptAnswer] = useState<PromptAnswer | null>(null);
 
-  function handleOpenDrawer() {
+  function handleOpenPromptAnswer(promptAnswer: PromptAnswer) {
+    setSelectedPromptAnswer(promptAnswer);
     setIsDrawerOpen(true);
   }
 
   function handleCloseDrawer() {
     setIsDrawerOpen(false);
+    setSelectedPromptAnswer(null);
   }
-
-  const promptAnswers: PromptAnswer[] = [
-    {
-      type: "freeform",
-      prompt: "Introduce yourself",
-      answer: "I am a programmer with 10+ years of experience.",
-    },
-    {
-      type: "freeform",
-      prompt: "What are you looking for in your next role?",
-      answer: "Fulfilling work",
-    },
-    {
-      type: "star",
-      prompt: "Tell me about a time you had a conflict with a co-worker.",
-      answer: {
-        situation: "",
-        task: "",
-        action: "",
-        result: "",
-      },
-    },
-  ];
 
   return (
     <>
       <h1>Behavioural Interviews</h1>
-      <h2>Introduction</h2>
-      <Table hoverable>
-        <TableHead>
-          <TableRow>
-            <TableHeadCell>Question</TableHeadCell>
-            <TableHeadCell>
-              <span className="sr-only">Answered?</span>
-            </TableHeadCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {promptAnswers.map((promptAnswer, index) => (
-            <TableRow key={index} role="button" onClick={handleOpenDrawer}>
-              <TableCell className="align-middle">
-                {promptAnswer.prompt}
-              </TableCell>
-              <TableCell>
-                <FaRegCircleCheck className="align-middle text-2xl" />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+
+      {promptAnswersByTheme.map(({ theme, promptAnswers }, index) => (
+        <Fragment key={index}>
+          <h2 className="mb-0">{theme}</h2>
+          <Table className="mt-2" hoverable>
+            <TableHead>
+              <TableRow>
+                <TableHeadCell>Prompt</TableHeadCell>
+                <TableHeadCell>
+                  <span className="sr-only">Answered?</span>
+                </TableHeadCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {promptAnswers.map((promptAnswer, index) => (
+                <TableRow key={index} role="button" onClick={() => handleOpenPromptAnswer(promptAnswer)}>
+                  <TableCell className="align-middle">
+                    {promptAnswer.prompt}
+                  </TableCell>
+                  <TableCell>
+                    <FaRegCircleCheck className="align-middle text-2xl" />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Fragment>
+      ))}
+
+      <h2 className="mb-0">Sources</h2>
+      <p>These prompts were inspired by:</p>
+      <ul>
+        <li><a href="https://www.techinterviewhandbook.org/behavioral-interview-questions/">Tech Interview Handbook</a></li>
+        <li><a href="https://www.themuse.com/advice/behavioral-interview-questions-answers-examples">the muse</a></li>
+        <li><a href="https://www.indeed.com/career-advice/interviewing/behavioral-interview-questions">indeed</a></li>
+        <li><a href="https://business.linkedin.com/talent-solutions/resources/interviewing-talent/behavioral-interview-questions-important-soft-skills">LinkedIn</a></li>
+      </ul>
 
       <Drawer
         open={isDrawerOpen}
         onClose={handleCloseDrawer}
         position="right"
-        backdrop={false}
         className={clsx("md:w-[400px]", isDrawerOpen && "shadow-[-8px_0_16px_-4px_rgba(0,0,0,0.3)]")}
       >
         <DrawerHeader title="Behavioural Question" />
         <DrawerItems>
-          <PromptAnswerForm promptAnswer={selectedPromptAnswer} />
+          {selectedPromptAnswer && (
+            <PromptAnswerForm promptAnswer={selectedPromptAnswer} />
+          )}
         </DrawerItems>
       </Drawer>
     </>

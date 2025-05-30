32825236814a41eac19b7a39252996b9f623965b
mattscripted <46871802+mattscripted@users.noreply.github.com>
2025-05-30 15:18:13 -0400

    Stub out behavioural interviews page

---
+++ src/app/behavioural-interviews/PromptAnswerForm.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  Label,
+  TextInput,
+  Textarea,
+} from "flowbite-react";
+import { PromptAnswer } from "@/types/prompt-answer";
+
+type PromptAnswerFormProps = {
+  promptAnswer: PromptAnswer
+}
+
+export default function PromptAnswerForm({
+  promptAnswer
+}: PromptAnswerFormProps) {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    alert('Not yet implemented!');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="max-w-md mb-4 font-bold">
+        {promptAnswer.prompt}
+      </div>
+      {promptAnswer.type === "freeform" && (
+        <div className="max-w-md mb-4">
+          <div className="mb-2 block">
+            <Label htmlFor="answer">Answer</Label>
+          </div>
+          <Textarea id="answer" rows={4} value={promptAnswer.answer} />
+        </div>
+      )}
+      {promptAnswer.type === "star" && (
+        <>
+          <div className="max-w-md mb-4">
+            <div className="mb-2 block">
+              <Label htmlFor="situation">Situation</Label>
+            </div>
+            <Textarea id="situation" rows={4} value={promptAnswer.answer.situation} />
+          </div>
+          <div className="max-w-md mb-4">
+            <div className="mb-2 block">
+              <Label htmlFor="task">Task</Label>
+            </div>
+            <Textarea id="task" rows={4} value={promptAnswer.answer.task} />
+          </div>
+          <div className="max-w-md mb-4">
+            <div className="mb-2 block">
+              <Label htmlFor="action">Action</Label>
+            </div>
+            <Textarea id="action" rows={4} value={promptAnswer.answer.action} />
+          </div>
+          <div className="max-w-md mb-4">
+            <div className="mb-2 block">
+              <Label htmlFor="result">Result</Label>
+            </div>
+            <Textarea id="result" rows={4} value={promptAnswer.answer.result} />
+          </div>
+        </>
+      )}
+    </form>
+  );
+}
--- src/app/behavioural-interviews/page.tsx
+++ src/app/behavioural-interviews/page.tsx
@@ -1,3 +1,106 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Table,
+  TableHead,
+  TableHeadCell,
+  TableBody,
+  TableRow,
+  TableCell,
+  Drawer,
+  DrawerHeader,
+  DrawerItems
+} from "flowbite-react";
+import clsx from "clsx";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import PromptAnswerForm from "./PromptAnswerForm";
+import { PromptAnswer } from "@/types/prompt-answer";
+
 export default function BehaviouralInterviewsPage() {
-  return <h1>Behavioural Interviews</h1>;
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  // TODO: Change selected prompt
+  const [selectedPromptAnswer] = useState<PromptAnswer>({
+    type: "star",
+    prompt: "Tell me about a time you had a conflict with a co-worker.",
+    answer: {
+      situation: "",
+      task: "",
+      action: "",
+      result: "",
+    },
+  });
+
+  function handleOpenDrawer() {
+    setIsDrawerOpen(true);
+  }
+
+  function handleCloseDrawer() {
+    setIsDrawerOpen(false);
+  }
+
+  const promptAnswers: PromptAnswer[] = [
+    {
+      type: "freeform",
+      prompt: "Introduce yourself",
+      answer: "I am a programmer with 10+ years of experience.",
+    },
+    {
+      type: "freeform",
+      prompt: "What are you looking for in your next role?",
+      answer: "Fulfilling work",
+    },
+    {
+      type: "star",
+      prompt: "Tell me about a time you had a conflict with a co-worker.",
+      answer: {
+        situation: "",
+        task: "",
+        action: "",
+        result: "",
+      },
+    },
+  ];
+
+  return (
+    <>
+      <h1>Behavioural Interviews</h1>
+      <h2>Introduction</h2>
+      <Table hoverable>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>Question</TableHeadCell>
+            <TableHeadCell>
+              <span className="sr-only">Answered?</span>
+            </TableHeadCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {promptAnswers.map((promptAnswer, index) => (
+            <TableRow key={index} role="button" onClick={handleOpenDrawer}>
+              <TableCell className="align-middle">
+                {promptAnswer.prompt}
+              </TableCell>
+              <TableCell>
+                <FaRegCircleCheck className="align-middle text-2xl" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <Drawer
+        open={isDrawerOpen}
+        onClose={handleCloseDrawer}
+        position="right"
+        backdrop={false}
+        className={clsx("md:w-[400px]", isDrawerOpen && "shadow-[-8px_0_16px_-4px_rgba(0,0,0,0.3)]")}
+      >
+        <DrawerHeader title="Behavioural Question" />
+        <DrawerItems>
+          <PromptAnswerForm promptAnswer={selectedPromptAnswer} />
+        </DrawerItems>
+      </Drawer>
+    </>
+  );
 }

85318983f302df86416bbd38a0c8eeb536bb75d2
mattscripted <46871802+mattscripted@users.noreply.github.com>
2025-06-03 19:48:42 -0400

    Limit matcher to allowed pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -55,14 +55,5 @@ export function middleware(request: NextRequest) {
  * Copied from: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
  */
 export const config = {
-  matcher: [
-    /**
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-  ],
+  matcher: ['/behavioural-interviews', '/404'],
 }

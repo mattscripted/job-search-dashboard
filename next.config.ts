0e4d9f572e106fbd3dd7a33ed8684133bd5ba7eb
mattscripted <46871802+mattscripted@users.noreply.github.com>
2025-06-09 17:47:13 -0400

    Remove all redirects (no longer used)

--- next.config.ts
+++ next.config.ts
@@ -6,37 +6,6 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  async redirects() {
-    return [
-      // Redirect MVP directly to behavioural interviews
-      {
-        source: '/',
-        destination: '/behavioural-interviews',
-        permanent: false,
-      },
-      // Hide previous pages from MVP
-      {
-        source: '/cheat-sheets',
-        destination: '/404',
-        permanent: false,
-      },
-      {
-        source: '/cheat-sheets/:slug',
-        destination: '/404',
-        permanent: false,
-      },
-      {
-        source: '/practice',
-        destination: '/404',
-        permanent: false,
-      },
-      {
-        source: '/resources',
-        destination: '/404',
-        permanent: false,
-      }
-    ]
-  }
 };
 
 const withMDX = createMDX({
